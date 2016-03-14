e4e545101245b2cc9a2795435b0a2e5c50c0b3cd
Nicholas <nicholas@Nicholass-MacBook-Air.local>
2016-03-14 11:27:12 -0700

    [feat] Starts about page

--- client/components/About.js
+++ client/components/About.js
@@ -3,8 +3,40 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
-        This is the about page.
+      <div className="container">
+      <h1 className="about-title">Meet The Team</h1>
+      <h5 className="about-minititle">[ Cogile NimbleKey Nimblr ]</h5>
+      <div className="row row-spacer"></div>
+      <div className="row">
+      <div className="col-md-3 about-borders">
+      <h3 className="about-names">Nicholas Der</h3>
+      <div className="row icon-container">
+      <a href="https://github.com/kiritsuzu" target="_blank"><i className="fa fa-github-square fa-5x about-icon"></i></a>
+      <a href="https://www.linkedin.com/in/kiritsuzu" target="_blank"><i className="fa fa-linkedin-square fa-5x about-icon"></i></a>
+      </div>
+      </div>
+      <div className="col-md-3 about-borders">
+      <h3 className="about-names">Rick Yeh</h3>
+      <div className="row icon-container">
+      <a href="https://github.com/kiritsuzu" target="_blank"><i className="fa fa-github-square fa-5x about-icon"></i></a>
+      <a href="https://www.linkedin.com/in/kiritsuzu" target="_blank"><i className="fa fa-linkedin-square fa-5x about-icon"></i></a>
+      </div>
+      </div>
+      <div className="col-md-3 about-borders">
+      <h3 className="about-names">Jordan Allen</h3>
+      <div className="row icon-container">
+      <a href="https://github.com/kiritsuzu" target="_blank"><i className="fa fa-github-square fa-5x about-icon"></i></a>
+      <a href="https://www.linkedin.com/in/kiritsuzu" target="_blank"><i className="fa fa-linkedin-square fa-5x about-icon"></i></a>
+      </div>
+      </div>
+      <div className="col-md-3 about-borders">
+      <h3 className="about-names">Mark Kim</h3>
+      <div className="row icon-container">
+      <a href="https://github.com/kiritsuzu" target="_blank"><i className="fa fa-github-square fa-5x about-icon"></i></a>
+      <a href="https://www.linkedin.com/in/kiritsuzu" target="_blank"><i className="fa fa-linkedin-square fa-5x about-icon"></i></a>
+      </div>
+      </div>
+      </div>
       </div>
     );
   }
