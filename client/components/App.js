946eff4288fb1e0620bfd69ef751768edb1139c3
RickYeh <RickYeh@users.noreply.github.com>
2016-03-14 15:51:19 -0700

    [add] adds simple nav bar with home and about page link using bootstrap

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,25 @@
-var React = require('react');
+import React, { Component } from 'react';
+import NavLink from './NavLink';
 
-var App = React.createClass({
-  render: function() {
+class App extends Component {
+  render() {
     return (
-      <div className='main-container'> Hi from App! 
+      <div className='main-container'>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <ul role="nav" className="nav navbar-nav navbar-right">
+              <li><NavLink to="/" className="nav-label" onlyActiveOnIndex>Home</NavLink></li>
+              <li><NavLink to="/about" className="nav-label">About</NavLink></li>
+            </ul>
+            <NavLink to="/" onlyActiveOnIndex className="sitename">Nimbler</NavLink>
+          </div>
+        </nav>
+
+        {/* Rendering the children of this route below */}
         {this.props.children}
       </div>
-    )
+    );
   }
-});
+};
 
-module.exports = App;
+export default App;
