98f4b7aac9f35b67ea8a6772a1b18869a9a5b6cb
Nicholas <nicholas@Nicholass-MacBook-Air.local>
2016-03-14 18:55:13 -0700

    [fix] deletes extra styles.css & updates css to assets

--- client/components/About.js
+++ client/components/About.js
@@ -5,28 +5,28 @@ var team = [{
   key: 1,
   name: "Nicholas Der",
   bio: "Placeholder Bio",
-  image: "../css/nick-profile.jpeg",
+  image: "../assets/nick-profile.jpeg",
   git: "https://github.com/kiritsuzu",
   linked: "https://www.linkedin.com/in/kiritsuzu"
 }, {
   key: 2,
   name: "Rick Yeh",
   bio: "Placeholder Bio",
-  image: "../css/rick-profile.jpeg",
+  image: "../assets/rick-profile.jpeg",
   git: "https://github.com/rickyeh",
   linked: "https://www.linkedin.com/in/rickyeh"
 }, {
   key: 3,
   name: "Jordan Allen",
   bio: "Placeholder Bio",
-  image: "../css/jordan-profile.jpeg",
+  image: "../assets/jordan-profile.jpeg",
   git: "https://github.com/jordanallen98",
   linked: "https://www.linkedin.com/in/jordandallen"
 }, {
   key: 4,
   name: "Mark Kim",
   bio: "Placeholder Bio",
-  image: "../css/mark-profile.jpeg",
+  image: "../assets/mark-profile.jpeg",
   git: "https://github.com/marksanghoonkim",
   linked: "https://www.linkedin.com/in/marksanghoonkim"
 }];
