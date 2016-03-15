0aee9c6f4f664edf5a37acc9f46744923ee7c731
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-15 11:23:10 -0700

    [feature] prevents easy copy/paste all, alerts when puzzle is completed

--- client/components/AceEditor.js
+++ client/components/AceEditor.js
@@ -22,17 +22,31 @@ class AceEditor extends Component {
     const node = ReactDOM.findDOMNode(this.refs.root);
     const editor = ace.edit(node);
     editor.setTheme("ace/theme/chrome");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.setShowPrintMargin(false);
     editor.setOptions({minLines: 25});
     editor.setOptions({maxLines: 50});
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.getSession().setTabSize(2);
+    editor.setShowPrintMargin(false);
+
+    var example = 'function add2(num) {\n  return num + 2;\n};';
 
     var that = this;
 
     editor.getSession().on("change", function() {
       var code = editor.getSession().getValue();
+      if (code === example) {
+        alert('completed!');
+      }
       that.setState({code});
       console.log(that.state.code);
+    });
+
+    // prevents copy pasting the whole thing
+    editor.on("paste", function(e) {
+      if (e.text === example) {
+        var shuffled = e.text.split('').sort(function(){return 0.5-Math.random()}).join('');
+        e.text = "Nice try, here's your copied text :P\n" + shuffled;
+      }
     });
   }
 
