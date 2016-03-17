de54186b58c406afaa051e655d86f06fb93c737d
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-17 15:31:44 -0700

    [add] creates socket on Multiplayer page, updates server.js for socket.io

--- client/components/Multiplayer.js
+++ client/components/Multiplayer.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
 class Multiplayer extends Component {
   constructor() {
     super();
 
+    let host = location.origin.replace(/^http/, 'ws');
+    let socket = io.connect(host);
+
     this.state = {
-      
+      socket
     };
   };
 
   render() {
     return (
       <div>
-        Multiplayer!
+        Multiplayer
       </div>
     )
   };
