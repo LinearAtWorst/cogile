b35aed2e7d39f5b3598603e7b7fbb3cbfe337736
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-17 20:05:20 -0700

    [add] allows disconnecting of socket when navigating away from Multiplayer

--- client/components/Multiplayer.js
+++ client/components/Multiplayer.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
+// var Socket = require('react-socket').Socket;
+
 
 class Multiplayer extends Component {
-  constructor() {
-    super();
-
-    let host = location.origin.replace(/^http/, 'ws');
-    let socket = io.connect(host);
+  constructor(props) {
+    super(props);
+    
+    // let host = location.origin.replace(/^http/, 'ws');
+    // let socket = io.connect(host);
 
     this.state = {
-      socket
+      // socket
     };
   };
+
+  componentDidMount() {
+
+    this.socket = io();
+
+    console.log(this.socket);
+  }
+
+  componentWillUnmount() {
+    this.socket.disconnect();
+  }
 
   render() {
     return (
       <div>
         Under construction...
+        { /*<Socket url="localhost:8080"/> */}
       </div>
     )
   };
