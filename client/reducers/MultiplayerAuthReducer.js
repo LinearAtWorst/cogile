b254faa9200ee2abc2bd0b150ec134ddc11bb750
kiritsuzu <nickd2312@gmail.com>
2016-03-28 16:51:30 -0700

    [fix] removes multiplayer auth reducer

--- client/reducers/MultiplayerAuthReducer.js
+++
@@ -1,9 +0,0 @@
-export default function(state = null, action) {
-  switch (action.type) {
-    case 'MULTIPLAYER_AUTH':
-      console.log('User tried to access multiplayer while not logged in.', action.payload);
-      return action.payload;
-  }
-
-  return state;
-}
