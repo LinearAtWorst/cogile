336a039ba366447a1541027e98b65014106868c9
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-28 19:09:03 -0700

    [fix] fixes nimblecode logo link focus bug

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,10 @@ import { getUsername, smashUser } from '../actions/index';
 import { bindActionCreators } from 'redux';
 import NavLink from './NavLink';
 import { Link } from 'react-router';
+import Radium from 'radium';
+import color from 'color';
+
+var RadiumLink = Radium(Link)
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +29,12 @@ class App extends Component {
         <nav className="navbar navbar-default">
           <div className="container-fluid">
             <div className="navbar-collapse navbar-responsive-collapse collapse in" aria-expanded="true">
-              <Link to="/" onlyActiveOnIndex className="sitename pull-left">nimblecode</Link>
+              <RadiumLink
+              to="/"
+              onlyActiveOnIndex
+              className="sitename pull-left"
+              style={styles.base}>
+              nimblecode</RadiumLink>
               <ul role="nav" className="nav navbar-nav navbar-right">
                 <li><NavLink to="singleplayer" className="nav-label" onlyActiveOnIndex>Singleplayer</NavLink></li>
                 <li><NavLink to="multiplayer" className="nav-label">Multiplayer</NavLink></li>
@@ -36,12 +45,12 @@ class App extends Component {
                   <li><NavLink to="login" className="nav-label">Login</NavLink></li>
                   : 
                   <li>
-                    <Link
+                    <RadiumLink
                       to="/"
                       className="nav-label"
                       onClick={this.smash.bind(this)}>
                       Logout
-                    </Link>
+                    </RadiumLink>
                   </li>
                 }
               </ul>
@@ -55,6 +64,22 @@ class App extends Component {
     );
   }
 };
+
+var styles = {
+  base: {
+    color: '#eee',
+
+    // Adding interactive state couldn't be easier! Add a special key to your
+    // style object (:hover, :focus, :active, or @media) with the additional rules.
+    ':hover': {
+      color: color('#eee').lighten(0.2).hexString()
+    },
+    ':focus': {
+      color: color('#eee').lighten(0.2).hexString(),
+      textDecoration: 'none'
+    }
+  }
+}
 
 function mapStateToProps(state) {
   return { SavedUsername: state.SavedUsername }
