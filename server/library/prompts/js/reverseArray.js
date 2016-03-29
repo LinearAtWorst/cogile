6528d7e4d2b611e096e2955bb0757a3e84e38bf7
RickYeh <RickYeh@users.noreply.github.com>
2016-03-29 10:35:27 -0700

    [add] added toy problem like prompts

---
+++ server/library/prompts/js/reverseArray.js
@@ -0,0 +1,7 @@
+function reverseArray(array) {
+    var reversedArray = [];
+    array.forEach(function (value, index, arr) {
+        reversedArray.unshift(value);
+    });
+    return reversedArray;
+}
