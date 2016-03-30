e37b16bb6847ffba2f21c39f23a28efd9f4d467e
jordanallen98 <jordan.allen98@gmail.com>
2016-03-30 11:45:32 -0700

    [fix] fixes bug of starting 2 servers running test

--- test/specs/test-server.js
+++ test/specs/test-server.js
@@ -1,19 +1,12 @@
 var chai = require('chai');
 var expect = chai.expect;
 var chaiHttp = require('chai-http');
-var server = require('../../server/server.js');
 var should = chai.should();
 var db = require('../../server/db/schema');
 var knex = require('knex');
 var userController = require('../../server/controllers/userController');
 
 chai.use(chaiHttp);
-
-describe('Server', function(){
-  it('should exists!', function(){
-    expect(!!server).to.be.true;
-  });
-});
 
 describe('userController', function(){
   it('should contain signin', function(){
@@ -31,18 +24,5 @@ xdescribe('Database', function(){
   it('should have a highscores table', function(){
     expect(true).to.be.true;
   });
-});
-
-describe('Promts requests', function(){
-  it('should send a prompt back',function(done){
-    chai.request(server)
-    .get('/api/getRandomPrompt')
-    .end(function(err, res){
-      res.should.have.status(200);
-      done();
-    });
-  });
-
-
 });
 
