f21e8926d038b6a0cc556ff864f6969a15a5904a
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-29 18:23:04 -0700

    [feature] updates timer and swalert to use hundrethSeconds

--- client/containers/Timer.js
+++ client/containers/Timer.js
@@ -9,6 +9,7 @@ class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      hundrethSeconds: 0,
       tenthSeconds: 0,
       seconds: 0,
       minutes: 0,
@@ -24,9 +25,15 @@ class Timer extends Component {
     });
 
     this.intervalID = setInterval(function() {
-      var tenthSeconds = this.state.tenthSeconds + 1;
+      var hundrethSeconds = this.state.hundrethSeconds + 1;
+      var tenthSeconds = this.state.tenthSeconds;
       var seconds = this.state.seconds;
       var minutes = this.state.minutes;
+
+      if (hundrethSeconds > 9) {
+        tenthSeconds++;
+        hundrethSeconds = 0;
+      }
 
       if (tenthSeconds > 9) {
         seconds++;
@@ -38,13 +45,16 @@ class Timer extends Component {
         seconds = 0;
       }
 
+      var totalTimeInSeconds = ((minutes * 60) + seconds + (tenthSeconds / 10) + (hundrethSeconds / 100)).toFixed(2);
+
       this.setState({
+        hundrethSeconds : hundrethSeconds,
         tenthSeconds : tenthSeconds,
         seconds : seconds,
         minutes: minutes,
-        message: minutes + ':' + seconds + '.' + tenthSeconds
+        message: totalTimeInSeconds + ' seconds'
       });
-    }.bind(this), 100);
+    }.bind(this), 10);
   } 
 
   componentDidUpdate() {
@@ -55,7 +65,7 @@ class Timer extends Component {
           minutes: 0,
           seconds: 0,
           tenthSeconds: 0,
-          message: '0:0.0',
+          message: '0.00',
           timerOn: false
         });
       }
@@ -67,6 +77,7 @@ class Timer extends Component {
       this.setState({timerOn : false});
       // this.props.saveTimeElapsed(this.state.tenthSeconds, this.state.seconds, this.state.minutes);
       var time = {
+        hundrethSeconds: this.state.hundrethSeconds,
         tenthSeconds: this.state.tenthSeconds,
         seconds: this.state.seconds,
         minutes: this.state.minutes
