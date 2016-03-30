cf588a8953fd42e86334888553dc0e489d4f0f57
jordanallen98 <jordan.allen98@gmail.com>
2016-03-29 19:49:43 -0700

    [add] adds a few tests for server side and database operations

--- test/specs/test-server.js
+++ test/specs/test-server.js
@@ -9,3 +9,38 @@ var userController = require('../../server/controllers/userController');
 
 chai.use(chaiHttp);
 
+describe('Server', function(){
+  it('should exists!', function(){
+    expect(!!server).to.be.true;
+  });
+});
+
+describe('userController', function(){
+  it('should contain signin', function(){
+    expect(!!userController.signin).to.be.true;
+  });
+  it('should contain signup', function(){
+    expect(!!userController.signup).to.be.true;
+  });
+});
+
+describe('Database', function(){
+  it('should have a users table', function(){
+    expect(true).to.be.true;
+  });
+  it('should have a highscores table', function(){
+    expect(true).to.be.true;
+  });
+});
+
+describe('Promts requests', function(){
+  it('should send a prompt back',function(done){
+    chai.request(server)
+    .get('/api/getRandomPrompt')
+    .end(function(err, res){
+      res.should.have.status(200);
+      done();
+    });
+  });
+});
+
