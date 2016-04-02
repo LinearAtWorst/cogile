8e877c9b7e1ac8e360d1c348f49c1082579b5ab3
Jordan Allen <jordan.allen98@gmail.com>
2016-04-02 15:45:40 -0700

    [content] add software engineer to my bio

--- client/components/About.js
+++ client/components/About.js
@@ -18,7 +18,7 @@ var team = [{
 }, {
   key: 3,
   name: "Jordan Allen",
-  bio: "Jordan owned his own business at 21, is a avid, self taught(mostly), guitarist, and also has owned and worked on a couple project cars(0 - 60 around 4 seconds). He loves traveling around the world every chance he gets.",
+  bio: "Jordan is a Software Engineer who owned his own business at 21, is a avid, self taught(mostly), guitarist, and also has owned and worked on a couple project cars(0 - 60 around 4 seconds). He loves traveling around the world every chance he gets.",
   image: "../assets/jordan-profile.jpg",
   git: "https://github.com/jordanallen98",
   linked: "https://www.linkedin.com/in/jordandallen"
